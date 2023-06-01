75e8f8685053255e2da98baf4a1c3311a86f7bec
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2023-06-01 12:53:57 +0300

    com1t

--- Lab15RPM.java
+++ Lab15RPM.java
@@ -1,67 +1,61 @@
 import java.io.*;
 import java.util.Scanner;
-
 public class Lab15RPM {
-    private double x;
-    private double y;
-
-    public void calculate() {
-        y = x - Math.sin(x);
-        System.out.println("y = " + y);
-    }
-
-    public void saveToFile() {
-        try {
-            FileWriter writer = new FileWriter("C:\\Users\\datch\\OneDrive\\Рабочий стол\\otvet.txt");
-            writer.write(Double.toString(x) + "\n");
-            writer.write(Double.toString(y) + "\n");
-            writer.close();
-            System.out.println("Информация занесена в otvet.txt");
-        } catch (IOException e) {
-            System.out.println("Не удалось сохранить в файл: " + e.getMessage());
-        }
-    }
-
-    public void loadFromFile() {
-        try {
-            FileReader reader = new FileReader("C:\\Users\\datch\\OneDrive\\Рабочий стол\\otvet.txt");
-            Scanner scanner = new Scanner(reader);
-            x = Double.parseDouble(scanner.nextLine());
-            y = Double.parseDouble(scanner.nextLine());
-            System.out.println("Информация загружена из файла.");
-        } catch (FileNotFoundException e) {
-            System.out.println("Файл не найден: " + e.getMessage());
-        } catch (IOException e) {
-            System.out.println("Неудалось загрузить информацию из файла: " + e.getMessage());
-        }
-    }
-
     public static void main(String[] args) {
-        Lab15RPM myObject = new Lab15RPM();
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner sc = new Scanner(System.in);
+        int otv = 0;
         while (true) {
-            System.out.print("Введите x.или же введите 'exit' чтобы завершить: ");
-            String input = scanner.nextLine();
-            if (input.equals("exit")) {
+            System.out.println("Введите число x");
+            if (sc.hasNextInt()) {
+                otv = sc.nextInt();
                 break;
-            }
-            try {
-                double x = Double.parseDouble(input);
-                myObject.x = x;
-                myObject.calculate();
-            } catch (NumberFormatException e) {
-                System.out.println("Неверное значение.Введите корректное значение");
-            }
-
-            System.out.print("Введите 'save' или 'upload' чтобы сохранить или загрузить данные в/из файла: ");
-            input = scanner.nextLine();
-            if (input.equals("save")) {
-                myObject.saveToFile();
-            } else if (input.equals("upload")) {
-                myObject.loadFromFile();
-                myObject.calculate();
+            } else {
+                System.out.println("Введено неверное значение");
+                sc.next();
             }
         }
+
+        System.out.println("Ответ:");
+        math obj = new math(otv);
+        obj.otvet();
+
+
+        System.out.println("Введите save что бы сохранить/Введите upload, что бы загрузить");
+        String vvod = "";
+        while (true){
+            vvod = sc.next();
+            if(vvod.equals("save")||vvod.equals("Save")){
+                try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("C:\\Users\\79623\\OneDrive\\Рабочий стол\\lab-15\\lab-15.txt"))){
+                    oos.writeObject(obj);
+                    oos.writeInt(otv);
+                    oos.writeDouble(obj.y);
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+            if(vvod.equals("upload")||vvod.equals("Upload")){
+                try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream("C:\\Users\\79623\\OneDrive\\Рабочий стол\\lab-15\\lab-15.txt"))){
+                    math secmath = (math)ois.readObject();
+                    secmath.otvet();
+                }
+                catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+    }
+}
+class math implements Serializable {
+    private double x;
+    public double y;
+    math(int x){
+        this.x=x;
+    }
+    void otvet(){
+        double y = x-Math.sin(x);
+        System.out.println(y);
     }
 }
